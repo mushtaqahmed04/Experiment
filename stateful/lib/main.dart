20a758f5851e14e03def9e4bbaa74eaeb6b4790b
Mushtaq Ahmed <mushtaqjugnu04@gmail.com>
2025-03-21 15:28:43 +0500

    testing

---
+++ stateful/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Experiment());
+}
+
+class Experiment extends StatefulWidget {
+  const Experiment({super.key});
+
+  @override
+  State<Experiment> createState() => _ExperimentState();
+}
+
+class _ExperimentState extends State<Experiment> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Text("Tap to increase $counter"),
+            TextButton(
+                onPressed: () {
+                  counter++;
+                  print(counter);
+                  setState(() {
+                    
+                  });
+                },
+                child: Text("+")),
+                TextButton(
+                onPressed: () {
+                  counter--;
+                  print(counter);
+                  setState(() {});
+                },
+                child: Text("-"))
+          ],
+        ),
+      ),
+    );
+  }
+}
